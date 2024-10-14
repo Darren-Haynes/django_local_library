d20ccfce663ab61010acf891ace5f2c3525d708d
Darren Haynes <darrenhaynes@zoho.com>
2024-10-13 22:40:11 -0500

    Created first view and html template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+    # Filter books containing the word "The"
+    books_with_the = Book.objects.filter(title__contains="The")
+    # books_with_the = ", ".join([book.title for book in books_filtered])
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+        "books_with_the": books_with_the,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "index.html", context=context)
